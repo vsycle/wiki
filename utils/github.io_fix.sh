b8c71511a950213e062395e41e2b2a3d4ca62273
Jonas Spanoghe <jonas@spanoghe.net>
2020-10-10 13:03:56 +0200

    Try out github pages fix

--- utils/github.io_fix.sh
+++ utils/github.io_fix.sh
@@ -1,2 +1,2 @@
-{ echo -n '- urlPrefix = "//"\n//- '; cat src/templates/mixins.pug; } >src/templates/mixins.new.pug
+{ echo -n '- urlPrefix = "https://vsycle.github.io/wiki/"\n//- '; cat src/templates/mixins.pug; } >src/templates/mixins.new.pug
 mv src/templates/mixins.new.pug src/templates/mixins.pug
