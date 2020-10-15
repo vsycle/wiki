b37c967dafec1097c9ce7714d1aa7da7389e5f65
Jonas Spanoghe <jonas@spanoghe.net>
2020-10-15 22:57:03 +0200

    Moved Cloud Run game assets to different folder

--- src/assets/unity/js/UnityProgress.js
+++ src/assets/unity/js/UnityProgress.js
@@ -6,7 +6,7 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
     unityInstance.container.appendChild(unityInstance.logo);
   }
-  if (!unityInstance.progress) {    
+  if (!unityInstance.progress) {
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
     unityInstance.progress.empty = document.createElement("div");
